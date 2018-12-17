fcfce23bfd644c9f9a020add406904d1a16341a4
behom002 <benedict.homuth@gmail.com>
2018-12-17 18:20:58 +0100

    First upload of Battleship game!

---
+++ Battleship.java
@@ -0,0 +1,22 @@
+
+
+public class SchiffeVersenken{
+
+public static int[][] createTableTop(int amountOfRows, int amountOfColumns){
+  int[amountOfRows][amountOfColumns] arr = {};
+
+  for(int i row = 0; i < amountOfRows; rows++){
+    for(int i column = 0; column < amountOfColumns; column++){
+      arr[row][column] = 0;
+    }
+  }
+}
+
+
+public static void main(String... args){
+
+
+
+}
+
+}
